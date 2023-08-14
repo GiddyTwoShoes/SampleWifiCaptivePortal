44faf6544df641b6fb17050b945102d0c756e772
GiddyTwoShoes <SAFWAN98.15@ichat.sp.edu.sg>
2023-08-14 21:38:10 +0800

    Add files via upload

---
+++ successpage.php
@@ -0,0 +1,152 @@
+
+  <html>
+
+  <head>
+
+    <title>SIM Wifi Hotspot - Sign In</title>
+
+    <meta charset='UTF-8'>
+    <meta http-equiv="cache-control" content="no-cache" />
+    <meta http-equiv="expires" content="0" />
+    <meta http-equiv="pragma" content="no-cache" />
+    <meta name="viewport" content="width=device-width,
+    initial-scale=0.75, maximum-scale=0.75, user-scalable=no">
+    <script src="jquery-2.2.1.min.js"></script>
+    <script src="jquery.validate.min.js"></script>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="icon" type="image/png" href="assets/img/profImage.png"/>
+
+    <style>
+
+  .container label.error {
+    margin-top: 4px;
+    color:red;
+    font-style: italic;
+		display: inline;
+
+	}
+
+
+
+  form.form-signin label.error {
+		display: block;
+		margin-left: 1em;
+
+		width: auto;
+	}
+    body {
+      background-image: url("assets/img/background.jpg");
+      background-position: center;
+      background-attachment: fixed;
+      background-repeat: no-repeat;
+      -webkit-background-size: 100%;
+      -moz-background-size: 100%;
+      -o-background-size: 100%;
+      background-size: 100%;
+      -webkit-background-size: cover;
+      -moz-background-size: cover;
+      -o-background-size: cover;
+      background-size: cover;
+    }
+
+    .google-header-bar {
+    height: 71px;
+    border-bottom: 1px solid #e5e5e5;
+    overflow: hidden;
+    }
+
+    .google-header-bar.centered {
+    border: 0;
+    height: 108px;
+    }
+
+    .google-header-bar.centered .header .logo {
+    float: none;
+    margin: 40px auto 30px;
+    display: block;
+    }
+    .google-header-bar.centered .header .secondary-link {
+    display: none
+    }
+
+    .header .logo {
+    margin: 17px 0 0;
+    float: left;
+    height: 38px;
+    width: 116px;
+    }
+    </style>
+
+    <style media="screen and (max-width: 800px), screen and (max-height: 800px)">
+      .google-header-bar.centered {
+        height: 83px;
+      }
+
+      .google-header-bar.centered .header .logo {
+        margin: 25px auto 20px;
+      }
+
+      .card {
+        margin-bottom: 20px;
+      }
+    </style>
+
+    <style media="screen and (max-width: 580px)">
+      html,
+      body {
+        font-size: 14px;
+      }
+
+      .google-header-bar.centered {
+        height: 73px;
+      }
+
+      .google-header-bar.centered .header .logo {
+        margin: 20px auto 15px;
+      }
+
+      .content {
+        padding-left: 10px;
+        padding-right: 10px;
+      }
+
+      .hidden-small {
+        display: none;
+      }
+
+      .card {
+        padding: 20px 15px 30px;
+        width: 270px;
+      }
+
+      .footer ul li {
+        padding-right: 1em;
+      }
+
+      .lang-chooser-wrap {
+        display: none;
+      }
+    </style>
+        
+  </head>
+
+  <body>
+
+    <div class="container">
+      <div class="account-wall">
+        <img class="profile-img" src="assets/img/logo.jpg" alt="logo.jpg"></img>
+
+        <h1 class="text-center login-title">SIM Wifi Hotspot</h1>
+        <h2 class="text-center friends-text">You are now authorized to access SIM Wifi</h2>
+
+          <form class="form-signin" id="captiveportalForm">
+
+
+            <div class="text-center terms-text1">Copyright @ 2019 - 2023 SIM, IT division. All Rights Reserved</div>
+
+          </form>
+
+  </body>
+
+</html>
